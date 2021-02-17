5f139f79c8383cdd43a53374a0e42a6fcefa923e
spiritofenergy <spiritofenergy220@gmail.com>
2021-02-18 01:13:22 +0300

    Create Grid And Grid Items

---
+++ app/src/main/java/com/kodexgroup/betonapp/utils/views/ButtonGridView.kt
@@ -0,0 +1,17 @@
+package com.kodexgroup.betonapp.utils.views
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.widget.LinearLayout
+import com.kodexgroup.betonapp.R
+
+class ButtonGridView(context: Context, attr: AttributeSet) : LinearLayout(context, attr) {
+
+    init {
+        val inflater = context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        val root = inflater.inflate(R.layout.content_search_item_grid_item, this, true)
+    }
+
+}
